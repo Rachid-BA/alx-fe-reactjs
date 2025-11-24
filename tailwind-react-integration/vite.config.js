34cc2b1a3a6b278e0262c79c12166d8ecc3f22c4
Rachid Bakkass <timitar.gasba@gmail.com>
2025-11-24 13:34:08 +0100

    Update vite.config.js

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 👈 هذا السطر مهم جداً
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 وهذا السطر أيضاً
+  ],
 })
