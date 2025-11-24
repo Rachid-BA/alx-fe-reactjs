08cc84483f781019bcbd0238d5d816099583a899
Rachid Bakkass <timitar.gasba@gmail.com>
2025-11-24 13:48:23 +0100

    Remove comments from Vite config for Tailwind CSS

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 👈 هذا السطر مهم جداً
+import tailwindcss from '@tailwindcss/vite' // 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 👈 وهذا السطر أيضاً
+    tailwindcss(), //  
   ],
 })
